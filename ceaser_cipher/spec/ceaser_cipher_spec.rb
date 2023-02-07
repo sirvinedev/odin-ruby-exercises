678d0898d93ff7ddb292bb99531ee2e760754d95
Sean Irvine <55446841+sirvinedev@users.noreply.github.com>
2023-02-07 23:17:15 +0000

    Added ceaser cipher exercise tests

---
+++ ceaser_cipher/spec/ceaser_cipher_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../ceaser_cipher'
+
+RSpec.describe 'Ceaser Cipher' do
+  describe 'lower case exercises' do
+    it 'returns a string shifted by 5 values' do
+      expect(ceaser_cipher("hello world", 5)).to eq("mjqqt btwqi")
+    end
+  end
+
+  describe 'upper case exercises' do
+    it 'returns a string shifted by 5 values' do
+      expect(ceaser_cipher("HELLO WORLD", 5)).to eq("MJQQT BTWQI")
+    end
+  end
+
+  describe 'mixed case exercises' do
+    it 'returns a string shifted by 5 values' do
+      expect(ceaser_cipher("HeLlO WoRlD", 5)).to eq("MjQqT BtWqI")
+    end
+  end
+
+  describe 'empty string exercises' do
+    it 'returns an empty string' do
+      expect(ceaser_cipher("", 5)).to eq("")
+    end
+  end
+end
