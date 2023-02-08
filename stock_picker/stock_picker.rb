eb14ffdbe1f13e7b6cabee67a2f10c9d59e1a3e9
Sean Irvine <55446841+sirvinedev@users.noreply.github.com>
2023-02-08 12:05:03 +0000

    Added stock_picker function

---
+++ stock_picker/stock_picker.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+def stock_picker(arr)
+  # arr1 = arr.each_with_index.to_a.combination(2).select {|buy, sell| buy[0] < sell[0]}
+  # max_profit = arr1.collect {|elem| elem[1][0] - elem[0][0]}.max
+  # arr2 = arr1.find {|elem| elem[1][0] - elem[0][0] == max_profit}.map {|elem| elem[1]}
+  arr.each_with_index
+     .to_a
+     .combination(2)
+     .max_by { |buy, sell| sell[0] - buy[0] }.map { |_profit, i| i }
+end
+
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
